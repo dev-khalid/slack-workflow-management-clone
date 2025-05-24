0eb79876bfb60949fc1afe49bf574c55cdf2863b
dev-khalid <khalid.hossain.akash@gmail.com>
2025-05-24 14:17:32 +0600

    chore: cleanup

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -14,17 +14,17 @@ export default defineConfig({
   driver: MySqlDriver,
   debug: true,
   migrations: {
-    tableName: 'migrations', // name of database table with log of executed transactions
-    path: process.cwd() + '/src/migrations', // path to the folder with migrations
-    pathTs: undefined, // path to the folder with TS migrations (if used, you should put path to compiled files in `path`)
-    glob: '!(*.d).{js,ts}', // how to match migration files (all .js and .ts files, but not .d.ts)
-    transactional: true, // wrap each migration in a transaction
-    disableForeignKeys: true, // wrap statements with `set foreign_key_checks = 0` or equivalent
-    allOrNothing: true, // wrap all migrations in master transaction
-    dropTables: true, // allow to disable table dropping
-    safe: false, // allow to disable table and column dropping
-    snapshot: true, // save snapshot when creating new migrations
-    emit: 'ts', // migration generation mode
+    tableName: 'migrations', 
+    path: process.cwd() + '/src/migrations', 
+    pathTs: undefined, 
+    glob: '!(*.d).{js,ts}', 
+    transactional: true, 
+    disableForeignKeys: true, 
+    allOrNothing: true, 
+    dropTables: true, 
+    safe: false, 
+    snapshot: true, 
+    emit: 'ts', 
     generator: TSMigrationGenerator,
   },
 });
